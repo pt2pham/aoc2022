7c5ef1cdc151e29eeaf5e36e123158c37627bb75
Phong Pham <phong.pham@shopify.com>
2022-12-01 22:14:11 -0500

    Reorganized sample data folder

--- day1/part_1.rb
+++ day1/part_1.rb
@@ -1,6 +1,6 @@
 require_relative "../elf_parser"
 require_relative "../calorie_calculator"
 
-elves = ElfParser.new("day1/elf_calories_sample.txt").parse
+elves = ElfParser.new("sample_data/day_1_calories.txt").parse
 
 puts CalorieCalculator.top_calories(1, elves).first
